6f90d7541f6e190f9e7cf2c02b472ae4a934dab1
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-25 10:39:06 +0100

    functions done

--- jQueryFunction.js
+++ jQueryFunction.js
@@ -1,1 +1,31 @@
-//! Jefferson  Bertinoti
+//! Jefferson    Bertinoti
+//todo Create and add an HTML element with any text value after another element
+var el = $("<span>" + 'text ' + "<span>");
+var div = $("#foo");
+$(el).insertAfter(div);
+
+//todo Create and add an HTML element with any text value before another element
+var el = $("<span>" + 'text ' + "<span>");
+var div = $("#foo");
+$(el).insertBefore(div);
+
+//todo Clone an HTML element within other element
+var newElement = $("element1").clone()
+
+//todo Add a class to an HTML item
+$( "myDiv" ).addClass( "mystyle" );
+
+//todo Remove a class to an HTML item
+$( "myDiv" ).removeClass("mystyle")
+
+//todo Toggle a class of an HTML item
+$("myDiv").toggleClass("mystyle");
+
+//todo Add a disabled attribute to an HTML button
+$("#myBtn").attr("disabled", true);
+
+//todo Remove the disabled attribute of an HTML button
+$("myBtn").attr("disabled", false);
+
+//todo Set a data-src attribute to a img element
+$('img').attr("data-src", "foobar");
--- javaScriptFunction.js
+++ javaScriptFunction.js
@@ -1,1 +1,41 @@
 //! Jefferson    Bertinoti
+//todo Create and add an HTML element with any text value after another element
+el = document.createElement("span");
+el.innerHTML = "text";
+var div = document.getElementById("foo");
+insertAfter(div, el);
+
+//todo Create and add an HTML element with any text value before another element
+el = document.createElement("span");
+el.innerHTML = "text";
+var div = document.getElementById("foo");
+insertBefore(div, el);
+
+//todo Clone an HTML element within other element
+var oldElement = document.querySelector('#element1');
+var newElement = oldElement.cloneNode(true);
+
+//todo Add a class to an HTML item
+var element = document.getElementById("myDiv");
+element.classList.add("mystyle");
+
+//todo Remove a class to an HTML item
+var element = document.getElementById("myDiv");
+element.classList.remove("mystyle");
+
+//todo Toggle a class of an HTML item
+var element = document.getElementById("myDiv");
+element.classList.toggle("mystyle");
+
+//todo Add a disabled attribute to an HTML button
+document.getElementById("myBtn").disabled = true;
+
+//todo Remove the disabled attribute of an HTML button
+document.getElementById('myBtn').disabled = false;
+
+//todo Set a data-src attribute to a img elementconst 
+var image = document.createElement('img');
+image.className = 'my-img';
+image.src = '/img/img1.jpg';
+image.setAttribute('data-src', 'foobar');
+document.body.appendChild(image);
