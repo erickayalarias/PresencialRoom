3f5ebe8d8c6e1fa114d9a958715e0cd48ef6732a
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-25 10:58:25 +0100

    indent

--- javaScriptFunction.js
+++ javaScriptFunction.js
@@ -1,40 +1,47 @@
 //!Erick Ayala first 8
-//When the HTML document has been loaded and you can manipulate it with JavaScript
+//todo When the HTML document has been loaded and you can manipulate it with JavaScript
 document.onload()
-//When an HTML item has been clicked
-document.addEventListener("click", ()=>{
+
+//todo When an HTML item has been clicked
+document.addEventListener("click", () => {
     console.log("estoy haciendo un click")
 })
-//When an HTML item has been double clicked
-document.addEventListener("dblclick", ()=>{
+
+//todo When an HTML item has been double clicked
+document.addEventListener("dblclick", () => {
     console.log("estoy haciendo doble-click")
 })
-//When the user presses a key on the keyboard
+
+//todo When the user presses a key on the keyboard
 window.addEventListener('keydown', function (e) {
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
-  }, false);
-//When the user moves the mouse cursor
+}, false);
+
+//todo When the user moves the mouse cursor
 document.onmousemove()
-// to see where the mouse moves
-onmousemove = function(e){console.log(`mouse location = X: ${e.x}, Y: ${e.y}`)}
-//When the user changes a value of an text input
-document.querySelector("input").addEventListener("change",function () {
+
+//todo  to see where the mouse moves
+onmousemove = function (e) { console.log(`mouse location = X: ${e.x}, Y: ${e.y}`) }
+
+//todo When the user changes a value of an text input
+document.querySelector("input").addEventListener("change", function () {
     alert("Input Changed");
 })
 
 document.getElementById("mytext").value = "My value"
 
-//When an image is loaded
+//todo When an image is loaded
 var img = document.querySelector('img')
 if (img.complete) {
-  console.log("imagen esta cargada")}
+    console.log("imagen esta cargada")
+}
 
-//version2 otra manera de hacerlo mas como te lo pide
+//todo version2 otra manera de hacerlo mas como te lo pide
 function testImage(URL) {
-    var tester=new Image();
-    tester.onload=imageFound;
-    tester.onerror=imageNotFound;
-    tester.src=URL;
+    var tester = new Image();
+    tester.onload = imageFound;
+    tester.onerror = imageNotFound;
+    tester.src = URL;
 }
 function imageFound() {
     alert('That image is found and loaded');
@@ -43,13 +50,13 @@ function imageNotFound() {
     alert('That image was not found.');
 }
 
-//When an image fails to load, (if you write an incorrect image url the loading of the image will fail)
+//todo When an image fails to load, (if you write an incorrect image url the loading of the image will fail)
 
 function testImage(URL) {
-    var tester=new Image();
-    tester.onload=imageFound;
-    tester.onerror=imageNotFound;
-    tester.src=URL;
+    var tester = new Image();
+    tester.onload = imageFound;
+    tester.onerror = imageNotFound;
+    tester.src = URL;
 }
 
 function imageFound() {
@@ -59,8 +66,6 @@ function imageFound() {
 function imageNotFound() {
     alert('That image was not found.');
 }
-
-
 
 
 //! Jefferson    Bertinoti
@@ -113,15 +118,15 @@ imgElement.removeAttribute('data-src');
 
 //todo hide
 var Elemen = document.getElementById('');
-GamepadButton.addEventListener("click",function(){Elemen.style.display = 'none';})
+GamepadButton.addEventListener("click", function () { Elemen.style.display = 'none'; })
 
 //todo show
-GamepadButton.addEventListener("click",function(){Elemen.style.display = 'block';})
+GamepadButton.addEventListener("click", function () { Elemen.style.display = 'block'; })
 
 //todo Iterate a collection of elements
 var arr = document.getElementById('');
 for (const a of arr) {
-    a.style.border=" 1px solid black"
+    a.style.border = " 1px solid black"
 }
 //todo Get the parent element
 var el = document.getElementById('');
@@ -132,4 +137,4 @@ var myElement = document.getElementById('');
 myElement.children;
 
 //todo Get all the elements that have a certain class
-var allclass=document.getElementsByClassName("class")
+var allclass = document.getElementsByClassName("class")
