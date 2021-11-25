40189a6520951e10cc2ffd17feccbd9854d034bd
rogerpuig <rogerpuigdemasa@gmail.com>
2021-11-25 10:28:11 +0100

    roger done

--- jQueryFunction.js
+++ jQueryFunction.js
@@ -1,3 +1,23 @@
 //! Jefferson  Bertinoti
 
 //!Roger puigdemasa
+//remove data-src attribute
+$(button).on("click",function(){$(imgElement).removeData('src');})
+//hide
+$(button).on("click",function(){$(selector).hide();})
+//show
+$(button).on("click",function(){$(selector).show();})
+//fadeIn
+$(button).on("click",function(){$(selector).fadeIn();})
+//fadeOut
+$(button).on("click",function(){$(selector).fadeOut();})
+//Iterate a collection of elements
+$(selector).each( function (indexInArray, valueOfElement) {
+    $(this).css({"borde":"1px solid black"})
+});
+//Get the parent element
+$(Selection).parent()
+//Get the parent element
+$(Selection).children()
+//Get all the elements that have a certain class
+$(".class")
